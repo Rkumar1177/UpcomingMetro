e1bd2a309f32dae4c668649441e17a35a6397a4d
Rkumar1177 <118461158+Rkumar1177@users.noreply.github.com>
2025-10-23 23:58:53 +0530

    Update app.js

--- app.js
+++ app.js
@@ -64,7 +64,7 @@ const ROUTE = [
   {name:"Phase 3",                    lat:28.3650,lng:76.9700},
   {name:"Millennium City Centre",     lat:28.3500,lng:76.9550},
   {name:"Sutta Break",                lat:13.148188,lng:77.619812},
-  {name:"Home",                       lat:13.146361545537234,lng:77.6202038543683},
+  {name:"Home",                       lat:13.14627,lng:77.62023},
   {name:"Home2",                      lat:13.1463161,lng:77.6202549}
 ];
 
@@ -187,4 +187,5 @@ document.getElementById('stopBtn').onclick = () => {
   cardBanner.style.display = 'none';          // hide card if visible
   log('Tracking stopped');
 };
+
 
